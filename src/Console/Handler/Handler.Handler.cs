1cd46f2e17b9b1d7da37f066c435213e1a0df4b0
pmosk <pm@ggroupp.ru>
2024-01-09 01:23:02 +0400

    Create the first version

---
+++ src/Console/Handler/Handler.Handler.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using GarageGroup.Infra;
+using Microsoft.Extensions.Logging;
+
+namespace GarageGroup.Platform.Ping;
+
+partial class PingHandler
+{
+    public async ValueTask<Result<Unit, Failure<HandlerFailureCode>>> HandleAsync(PingIn? input, CancellationToken cancellationToken)
+    {
+        if (input?.HealthCheckUrl is null)
+        {
+            return Failure.Create(HandlerFailureCode.Persistent, "HealthCheckUrl must be specified");
+        }
+
+        var retryDelayInSeconds = input.RetryDelayInSeconds > 0 ? input.RetryDelayInSeconds.Value : DefaultRetryDelayInSeconds;
+        var maxAttempts = input.MaxAttempts > 0 ? input.MaxAttempts.Value : DefaultMaxAttempts;
+
+        for (var i = 0; i < maxAttempts; i++)
+        {
+            logger.LogInformation("Attempt {attemmpt} of {maxAttempts}", i + 1, maxAttempts);
+            var result = await PingAsync(input, cancellationToken).ConfigureAwait(false);
+
+            if (result.IsSuccess)
+            {
+                return Result.Success<Unit>(default);
+            }
+
+            var failure = result.FailureOrThrow();
+            logger.LogError(failure.SourceException, "{failureMessage}", failure.FailureMessage);
+
+            logger.LogInformation("Delay {delay}s", retryDelayInSeconds);
+            await Task.Delay(retryDelayInSeconds * 1000, cancellationToken).ConfigureAwait(false);
+        }
+
+        return Failure.Create(HandlerFailureCode.Persistent, "All attempts were unsuccessful");
+    }
+
+    private async ValueTask<Result<Unit, Failure<Unit>>> PingAsync(PingIn input, CancellationToken cancellationToken)
+    {
+        try
+        {
+            using var httpClient = new HttpClient(httpMessageHandler, disposeHandler: false);
+            using var httpResponse = await httpClient.GetAsync(input.HealthCheckUrl, cancellationToken).ConfigureAwait(false);
+
+            var httpResponseBody = await httpResponse.Content.ReadAsStringAsync(cancellationToken).ConfigureAwait(false);
+            logger.LogInformation("{responseBody}", httpResponseBody);
+
+            if (httpResponse.IsSuccessStatusCode is false)
+            {
+                return Failure.Create("An unsuccessful HTTP request");
+            }
+
+            var containedMessage = input.ContainedMessage;
+            if (string.IsNullOrWhiteSpace(containedMessage) || httpResponseBody.Contains(containedMessage, StringComparison.InvariantCulture))
+            {
+                return Result.Success<Unit>(default);
+            }
+
+            return Failure.Create("Response does not contain the required message");
+        }
+        catch (OperationCanceledException ex)
+        {
+            return ex.ToFailure("An HTTP request was canceled");
+        }
+    }
+}
---
+++ src/Console/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using GarageGroup.Infra;
+
+namespace GarageGroup.Platform.Ping;
+
+public static class Program
+{
+    public static Task Main(string[] args)
+        =>
+        Application.UsePingHandler().RunConsoleAsync("In", args);
+}
